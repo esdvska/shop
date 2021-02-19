e251cce13690674ca4435de17d147766ea2a3e55
Ewelina Sadowska <esdvska@gmail.com>
2021-02-19 11:37:37 +0100

    logic add

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -6,8 +6,10 @@ import { AppContext } from "../AppContext";
 const Catalog = () => {
   const { products } = useContext(AppContext);
 
-  console.log(products);
   const [inputValue, setInputValue] = useState("");
+
+  const [radioValue, setRadioValue] = useState("all");
+
   let manArr = [];
   let twiced = [];
 
@@ -19,12 +21,21 @@ const Catalog = () => {
     }
   }
 
-  console.log(manArr);
+  const showManufacturer = (e) => {
+    setInputValue("");
+    setRadioValue(e.target.value);
+  };
   const manufacturers = manArr.map((product) => {
     return (
-      <div>
-        <input type="radio" name="manufacturere" id={product} value={product} />
-        <label for={product}> {product}</label>
+      <div key={product}>
+        <input
+          type="radio"
+          name="manufacturere"
+          id={product}
+          value={product}
+          onChange={showManufacturer}
+        />
+        <label htmlFor={product}> {product}</label>
       </div>
     );
   });
@@ -38,6 +49,20 @@ const Catalog = () => {
       </div>
     );
   });
+
+  const searchProRadio = products.filter((product) => {
+    return product.manufacture === radioValue;
+  });
+  const searchRadio = searchProRadio.map((product) => {
+    return (
+      <div key={product.id} className="product">
+        <img src={product.image} alt={product.name} />
+        <p className="price">{product.amount}</p>
+        <p>{product.name}</p>
+      </div>
+    );
+  });
+  const radioProducts = radioValue === "all" ? allProducts : searchRadio;
 
   const searchProducts = products.filter((product) =>
     product.name.toLowerCase().includes(inputValue.toLowerCase())
@@ -55,22 +80,22 @@ const Catalog = () => {
 
   const handleSearch = (e) => {
     setInputValue(e.target.value);
+    manufacturers.forEach((manufacturer) => manufacturer.checked === false);
   };
 
-  const displayProducts = inputValue ? displaySearchProducts : allProducts;
+  const displayProducts = inputValue ? displaySearchProducts : radioProducts;
   return (
     <div className="container">
-      <h1 class="header-big">Catalog</h1>
-      <div class="catalog">
+      <h1 className="header-big">Catalog</h1>
+      <div className="catalog">
         <div className="column-left">
           <div className="filter">
             <div className="filter-header">
               {" "}
               <h4>Search</h4>
-              <span class="clear">Clear</span>
+              <span className="clear">Clear</span>
             </div>{" "}
             <div>
-              {" "}
               <input
                 type="text"
                 placeholder="search..."
@@ -86,15 +111,15 @@ const Catalog = () => {
                   name="manufacturere"
                   id="all"
                   value="all"
-                  checked
-                />{" "}
-                <label for="all">All</label>
+                  onChange={showManufacturer}
+                />
+                <label htmlFor="all">All</label>
               </div>
               {manufacturers}
             </div>
           </div>
         </div>
-        <div class="column-right">
+        <div className="column-right">
           <div className="products">{displayProducts}</div>
         </div>
       </div>
